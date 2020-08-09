4138f4e666582b37d755876f4fecf799db4d0e24
Aleks12345678 <aleksreut@yandex.ru>
2020-08-09 17:05:40 +0500

    выполнено первое задания первого урока

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
-alert('Hello world!');
-alert('1');
-console.log(2);
+let money = 1;
+let income = 2;
+let addExpenses = 3;
+let deposit = 4;
+let mission = 5;
+let period = 6;
+
+alert(money);
+console.log(addExpenses);
+console.log('hello world');
